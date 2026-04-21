e48ca0b2c6649ea6f27e85d8a8122ad5923c1156
Hisham <hisham@catsplatstudios.com>
2026-04-21 08:46:00 -0400

    Lab 3 partway

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class PlayerController : MonoBehaviour
 {
+
+    //public GameObject groundCheckTransform;
+    [SerializeField] private float groundCheckRadius = 0.02f;
+    [SerializeField] private LayerMask groundLayer;
+
     //Configurable variables
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
@@ -10,23 +15,48 @@ public class PlayerController : MonoBehaviour
     //1. Poll our input so that we can see what our input values are.
     //2. Move our player horizontally based on the horizontal input value.
     private Rigidbody2D rb;
+    private Collider2D col;
+
+    //cached variables
+    private Vector2 groundCheckPos => CalculateGroundCheckPos();
+
+    //state variables
+    private bool _isGrounded;
+
+    private Vector2 CalculateGroundCheckPos()
+    {
+        Bounds bounds = col.bounds;
+        return new Vector2(bounds.center.x, bounds.min.y);
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+
+        //if (groundCheckTransform == null)
+        //{
+        //    Debug.LogError("Ground Check Transform is not assigned in the inspector.");
+        //    groundCheckTransform = new GameObject("GroundCheck");
+        //    groundCheckTransform.transform.SetParent(transform);
+        //    groundCheckTransform.transform.localPosition = Vector3.zero;
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
+        _isGrounded = Physics2D.OverlapCircle(groundCheckPos, groundCheckRadius, groundLayer);
+
         float horizontalInput = Input.GetAxis("Horizontal");
         bool jumpInput = Input.GetButtonDown("Jump");
 
         // Move the player horizontally based on the horizontal input value
         rb.linearVelocityX = horizontalInput * moveSpeed;
 
-        if (jumpInput)
+        if (jumpInput && _isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
